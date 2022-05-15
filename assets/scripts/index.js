e3eb30bf2c192fe64a9c6bb1edf9af6a0d19e7c7
Jan Martinek <honza.martinek@gmail.com>
2022-05-15 12:48:29 +0200

    Zoom-in on images

---
+++ assets/scripts/index.js
@@ -0,0 +1,10 @@
+document.addEventListener('click', (e) => {
+  const wrapper = e.target.closest('.zoomed-in');
+  if (wrapper) wrapper.parentNode.removeChild(wrapper);
+  else if (e.target.tagName === 'IMG') {
+    const wrapper = document.createElement('DIV');
+    wrapper.classList.add('zoomed-in');
+    wrapper.appendChild(e.target.cloneNode());
+    document.body.appendChild(wrapper);
+  }
+});
