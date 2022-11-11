903321beee09d0c3d7f460d1c6213a1026594f2a
Jan Martinek <honza.martinek@gmail.com>
2022-11-11 08:52:45 +0100

    Fixes

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,7 +1,7 @@
 document.querySelector("article").addEventListener("click", (e) => {
   const wrapper = e.target.closest(".zoomed-in");
   if (wrapper) wrapper.parentNode.removeChild(wrapper);
-  else if (e.target.tagName === "IMG") {
+  else if (e.target.tagName === "IMG" && !/svg$/.test(e.target.src)) {
     const wrapper = document.createElement("DIV");
     wrapper.classList.add("zoomed-in");
     wrapper.appendChild(e.target.cloneNode());
